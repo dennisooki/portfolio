aca1d9b13004edf9aa6a4d4a051828df7dfd8edc
Magolo Dennis Ooki <dennisooki@gmail.com>
2026-02-19 13:39:27 +0300

    feat: assemble all components into the main page

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Hero from "./components/Hero";
+import Philosophy from "./components/Philosophy";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Credentials from "./components/Credentials";
+import Contact from "./components/Contact";
+import LaFirma from "./components/LaFirma";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Philosophy />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Credentials />
+      <Contact />
+      <LaFirma />
+    </>
+  );
+}
